420ff8373c896bc43d711cd57bfe398b45cb2eb9
Basilio Lopez <blopez291@toromail.lausd.edu>
2024-02-17 15:08:14 -0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HelloPage } from './Job.js';
+
+const Stack = createNativeStackNavigator();
+
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title='Go to Profile'
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  )
+}
+
+function ProfileScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Profile Screen</Text>lear
+      
+      <Button
+        title='Goto Hello'
+        onPress={() => navigation.navigate('Hello')}
+      />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name='Profile' component={ProfileScreen} />
+        <Stack.Screen name='Hello' component={HelloPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
