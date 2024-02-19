93cfc164b2b703491a83f651512b8533967ecef9
Shawn <shawnmerana@gmail.com>
2024-02-18 22:07:59 -0800

    Layout
    Basic layout of text and inputs
    Setup for future database
    
    Signed-off-by: Shawn <shawnmerana@gmail.com>

--- App.js
+++ App.js
@@ -1,46 +1,15 @@
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HelloPage } from './Login.js';
+import { LoginPage } from './Login';
 
 const Stack = createNativeStackNavigator();
-
-function Home({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title='Go to Profile'
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
-  )
-}
-
-function ProfileScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Profile Screen</Text>
-      <Button
-        title='Goto Hello'
-        onPress={() => navigation.navigate('Hello')}
-      />
-    </View>
-  )
-}
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen name='Profile' component={ProfileScreen} />
-        <Stack.Screen name='Hello' component={HelloPage} />
+        <Stack.Screen name='Login' component={LoginPage} options={{title: 'Login'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Login.js
+++ Login.js
@@ -1,11 +1,58 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, TextInput, SafeAreaView, StyleSheet, Button } from 'react-native';
 
+export function LoginPage({navigator}) {
 
-export function HelloPage({navigator}) {
+    const [username, setName] = useState('');
+    const [password, setPass] = useState('');
+    const [rejectNotif, setRejection] = useState('');
+    let validate = false;
+    
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Hello there</Text>
-        </View>
-    )
-}
+        <SafeAreaView style={{padding: 10, alignContent: 'center'}}>
+            <Text style={{color: 'red', alignSelf: 'center', }} textBreakStrategy='simple'>
+                {rejectNotif}
+            </Text>
+            <TextInput
+                style={styles.input}
+                autoComplete='username'
+                placeholder='Username'
+                onChangeText={newName => setName(newName)}
+                defaultValue={username}
+            />
+            <TextInput
+                style={styles.input}
+                autoComplete='current-password'
+                secureTextEntry={true}
+                placeholder='Password'
+                onChangeText={newPass => setPass(newPass)}
+                defaultValue={password}
+            />
+            <Button
+                onPress={() => {
+                    //TODO: Add validation with a database for user logins
+                    // Use validate as return variable
+                    
+                    //validate = database.password() == password
+
+                    if (validate) {
+                        //navigator.navigate('Home');
+                    } else {
+                        // Notify the user of a failed login
+                        setRejection('Login information is incorrect  ');
+                    }                    
+                }}
+                title='Submit'
+            />
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
