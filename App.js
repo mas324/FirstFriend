0032994389d802f1c713916a7cf43d9d207a67a7
Shawn Merana <shawnmerana@gmail.com>
2024-03-09 11:52:19 -0800

    Authentication
    Basic integration of authentication flow
    
    Signed-off-by: Shawn Merana <shawnmerana@gmail.com>

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as SecureStore from 'expo-secure-store';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginPage } from './screens/Login';
@@ -9,21 +10,86 @@ import { Jobs } from './screens/Job';
 import Messages from './screens/Messages';
 import { SignUpPage } from './screens/Signup';
 
+export const AuthContext = React.createContext();
 const Stack = createNativeStackNavigator();
 
 export default function App({ navigation }) {
-  // Change route name to Test for development, Login for production
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+    const bootstrapAsync = async () => {
+      let userToken;
+      try {
+        userToken = await SecureStore.getItemAsync('userToken');
+      } catch (e) {
+        // When failed
+      }
+
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(() => ({
+    signIn: async (data) => {
+      console.log(data);
+      dispatch({ type: 'SIGN_IN', token: 'token' });
+    },
+    signOut: () => dispatch({ type: 'SIGN_OUT' }),
+    signUp: async (data) => {
+      dispatch({ type: 'SIGN_IN', token: 'token' });
+    },
+  }), []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Test' component={Test} options={{title: 'Dev Test page'}} />
-        <Stack.Screen name='Login' component={LoginPage} options={{title: 'Login'}} />
-        <Stack.Screen name='PassRes' component={PasswordReset} options={{title: 'Forgot Password'}} />
+      <AuthContext.Provider value={authContext}>
+        <Stack.Navigator initialRouteName='Login'>
+        {state.userToken == null ? (
+          <>
+            <Stack.Screen name='Login' component={LoginPage} options={{title: 'Login'}} />
+            <Stack.Screen name='PassRes' component={PasswordReset} options={{title: 'Forgot Password'}} />
         <Stack.Screen name='Signup' component={SignUpPage} options={{title: 'Signup'}} />
         <Stack.Screen name='Home' component={Home} options={{title: 'Home'}} />
         <Stack.Screen name='Jobs' component={Jobs} options={{title: 'Jobs'}} />
         <Stack.Screen name='Messages' component={Messages} options={{title: 'Messages'}} />
-      </Stack.Navigator>
+          </>
+        ) : (
+          <>
+            <Stack.Screen name='Test' component={Test} options={{title: 'Dev Test page'}} />
+          </>
+        )}
+        </Stack.Navigator>
+      </AuthContext.Provider>
     </NavigationContainer>
   );
 }
