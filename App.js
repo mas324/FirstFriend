a93af6e3169c825f9aab1dfde2984b3746338d82
Shawn Merana <shawnmerana@gmail.com>
2024-03-20 23:07:15 -0700

    Rebase fix
    Fixed up the effects of rebase to dev
    
    Signed-off-by: Shawn Merana <shawnmerana@gmail.com>

--- App.js
+++ App.js
@@ -9,67 +9,9 @@ import Home from './screens/Home';
 import { Jobs } from './screens/Job';
 import Messages from './screens/Messages';
 import { SignUpPage } from './screens/Signup';
-import { AuthContext } from './components/Auth';
+import { AuthContext } from './utils/Auth';
 
 const Stack = createNativeStackNavigator();
-
-export default function App({ navigation }) {
-  const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case 'RESTORE_TOKEN':
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case 'SIGN_IN':
-          return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-          };
-        case 'SIGN_OUT':
-          return {
-            ...prevState,
-            isSignout: true,
-            userToken: null
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: false,
-      userToken: null,
-    }
-  );
-
-  React.useEffect(() => {
-    const bootstrapAsync = async () => {
-      let userToken;
-      try {
-        userToken = await SecureStore.getItemAsync('userToken');
-      } catch (e) {
-        // When failed
-      }
-
-      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const AuthMemo = React.useMemo(() => ({
-    signIn: async (data) => {
-      console.log('Sign in data package: ', data);
-      dispatch({ type: 'SIGN_IN', token: 'token' });
-    },
-    signOut: () => dispatch({ type: 'SIGN_OUT' }),
-    signUp: async (data) => {
-      console.log('Sign up data package: ', data);
-      dispatch({ type: 'SIGN_IN', token: 'token' });
-    },
-  }), []);
 
 export default function App({ navigation }) {
   const [state, dispatch] = React.useReducer(
@@ -132,19 +74,20 @@ export default function App({ navigation }) {
   return (
     <NavigationContainer>
       <AuthContext.Provider value={AuthMemo}>
-        <Stack.Navigator initialRouteName='Login'>
+        <Stack.Navigator initialRouteName='Test'>
         {state.userToken == null || state.userToken != null ? (
           <>
             <Stack.Screen name='Login' component={LoginPage} options={{title: 'Login'}} />
             <Stack.Screen name='PassRes' component={PasswordReset} options={{title: 'Forgot Password'}} />
             <Stack.Screen name='Signup' component={SignUpPage} options={{title: 'Signup'}} />
-          </>
-        ) : (
-          <>
             <Stack.Screen name='Test' component={Test} options={{title: 'Dev Test page'}} />
             <Stack.Screen name='Home' component={Home} options={{title: 'Home'}} />
             <Stack.Screen name='Jobs' component={Jobs} options={{title: 'Jobs'}} />
-            <Stack.Screen name='Messages' component={Messages} options={{title: 'Messages'}} />
+            <Stack.Screen name='Messages' component={Messages} options={{title: 'Messages'}} />            
+          </>
+        ) : (
+          <>
+
           </>
         )}
         </Stack.Navigator>
