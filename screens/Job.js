2dce8de84e4e339d14793e5bc9bb21db97017cdb
Basilio Lopez <blopez291@toromail.lausd.edu>
2024-04-17 12:47:21 -0700

    added jobpostings

--- screens/Job.js
+++ screens/Job.js
@@ -6,6 +6,8 @@ import { jobStyles } from '../components/JobStyles';
 import Axios from 'axios';
 import { appStyles } from '../components/AppStyles';
 import { getItem, setItem } from '../utils/Authentication/LocalStore';
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+
 
 // DEMO ONLY
 const DemoCard = () => {
@@ -23,14 +25,36 @@ const DemoCard = () => {
     );
 }
 
-const instance = Axios.create({
-    //baseURL: 'https://api.coresignal.com/cdapi/v1/linkedin/job/collect/',
-    timeout: 1000,
-    headers: {
-        "Content-Type": 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJFZERTQSIsImtpZCI6IjY0NWVmNzg3LTZkNmMtZTQ2ZS1kNjRiLWQ0N2FkZWRkZGM4NSJ9.eyJhdWQiOiJ0b3JvbWFpbC5jc3VkaC5lZHUiLCJleHAiOjE3NDM2NDU5NjQsImlhdCI6MTcxMjA4OTAxMiwiaXNzIjoiaHR0cHM6Ly9vcHMuY29yZXNpZ25hbC5jb206ODMwMC92MS9pZGVudGl0eS9vaWRjIiwibmFtZXNwYWNlIjoicm9vdCIsInByZWZlcnJlZF91c2VybmFtZSI6InRvcm9tYWlsLmNzdWRoLmVkdSIsInN1YiI6ImZhMGM0YzljLWMyMWMtZmZkZi1jMGI5LTQ4YWVkNWFmOWMxNiIsInVzZXJpbmZvIjp7InNjb3BlcyI6ImNkYXBpIn19.npUu-sntifY5L1IdkUez1Lw_btDeSOoDyoFrJmZ0dYcK0jECdFa6RJrnHfp30WQVd36x02NTZtoJ59LMCHEUBg',
-    }
-})
+const Card = () => {
+    return (
+        <View>
+            <Text style={jobStyles.jobTitle}>Intern Front End Developer</Text>
+            <Text style={jobStyles.jobSection}>Description</Text>
+            <Text>• Development and design of Web based UI solutions to deliver an intuitive user experience</Text>
+            <Text>• Collaborate with users, technical, and architecture teams to solve complex user interface problems</Text>
+            <Text style={jobStyles.jobSection}>Qualifications</Text>
+            <Text>Bachelor, Master or Doctorate of Science degree from an accredited course of study, in engineering, computer science, mathematics, physics or chemistry.</Text>
+            <Text style={jobStyles.jobSection}>Salary</Text>
+            <Text>$20 per hour</Text>
+        </View>
+    );
+}
+
+// const getCard = () => {
+//     fetch("../assets/job_postings.json")
+//         .then(response => response.json())
+//         .then(values => values.forEach(value => console.log(value.title)))
+// }
+
+
+// const instance = Axios.create({
+//     //baseURL: 'https://api.coresignal.com/cdapi/v1/linkedin/job/collect/',
+//     timeout: 1000,
+//     headers: {
+//         "Content-Type": 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJFZERTQSIsImtpZCI6IjY0NWVmNzg3LTZkNmMtZTQ2ZS1kNjRiLWQ0N2FkZWRkZGM4NSJ9.eyJhdWQiOiJ0b3JvbWFpbC5jc3VkaC5lZHUiLCJleHAiOjE3NDM2NDU5NjQsImlhdCI6MTcxMjA4OTAxMiwiaXNzIjoiaHR0cHM6Ly9vcHMuY29yZXNpZ25hbC5jb206ODMwMC92MS9pZGVudGl0eS9vaWRjIiwibmFtZXNwYWNlIjoicm9vdCIsInByZWZlcnJlZF91c2VybmFtZSI6InRvcm9tYWlsLmNzdWRoLmVkdSIsInN1YiI6ImZhMGM0YzljLWMyMWMtZmZkZi1jMGI5LTQ4YWVkNWFmOWMxNiIsInVzZXJpbmZvIjp7InNjb3BlcyI6ImNkYXBpIn19.npUu-sntifY5L1IdkUez1Lw_btDeSOoDyoFrJmZ0dYcK0jECdFa6RJrnHfp30WQVd36x02NTZtoJ59LMCHEUBg',
+//     }
+// })
 
 const API_ENDPOINT = 'https://api.coresignal.com/cdapi/v1/linkedin/job/search/filter';
 
@@ -41,6 +65,7 @@ export function Jobs({ navigator }) {
 
     const onClickHandler = () => {
         console.log("Search Word = " + searchWord)
+                
         const searchRequest = { 
             "experience_title": searchWord,
             "country": '(United States)',
@@ -56,6 +81,7 @@ export function Jobs({ navigator }) {
                 console.error(err);
             })
         }
+        
     }
 
     return (
@@ -79,4 +105,3 @@ export function Jobs({ navigator }) {
         </SafeAreaView>
     );
 };
-// onPress={() => { (query) => handleSearch(query)}}
