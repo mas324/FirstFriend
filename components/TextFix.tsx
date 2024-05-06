10d859789357bbc1ae9b43583fa99394b526297f
Shawn Merana <shawnmerana@gmail.com>
2024-05-06 01:51:48 -0700

    reapply text fix

--- components/TextFix.tsx
+++ components/TextFix.tsx
@@ -10,9 +10,9 @@ export class Text extends React.Component<Native.TextProps> {
         {
           ...Native.Platform.select({
             android: {
-              // fontFamily: 'monospace',
-              alignSelf: 'stretch',
-              textAlign: 'center',
+              fontFamily: 'monospace',
+              // alignSelf: 'stretch',
+              // textAlign: 'center',
             },
           })
         }]} {...rest}>
